a168e55f59d796d72eb08099b0e93a3a351e0e26
1-WOOOPER-1 <v123vladimir@gmail.com>
2025-02-08 13:10:12 +0300

    Sepation of quote and author and displaying them on different lines

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 const quotes = [
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, quo.",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum adipisci rerum laudantium? Iure!",
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae minima cum ratione quo officia aspernatur soluta veniam! Aspernatur, cum facere?",
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    quote: "Be the change that you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "The only thing we have to fear is fear itself.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
 ];
 
-const output = document.getElementById("output");
+const quoteParagraph = document.getElementById("quote");
+const quoteAuthorParagraph = document.getElementById("quote-author");
 const button = document.getElementById("generate-button");
 
 const generateRandomQuote = () => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  output.textContent = randomQuote;
+  const { quote, author } = quotes[randomIndex];
+  quoteParagraph.innerHTML = quote;
+  quoteAuthorParagraph.innerHTML = author;
 };
 
 button.addEventListener("click", generateRandomQuote);
-
-// generateRandomQuote();
